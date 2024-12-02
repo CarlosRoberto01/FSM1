9195dc5d641b7f1c1136da781c1694c21bd6b251
Carlos Roberto <carloos.bezerra@hotmail.com>
2024-12-02 15:34:05 -0300

    Atividade dia 02 de dezembro

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
-console.log("Hello World!!!!");
+var primeiroNome = "Carlos ";
+var segundoNome = "Roberto ";
+var terceiroNome = "Alves ";
+var quartoNome = "Bezerra ";
+var quintoNome = "Luna";
+var idade = "30 anos";
 
-alert("Hello World!!!!");
+var nomeCompleto = primeiroNome + segundoNome + terceiroNome + quartoNome + quintoNome;
+
+console.log(nomeCompleto);
+
+// alert(nomeCompleto);
+
+document.write(nomeCompleto);
+
+console.log(`Olá ${nomeCompleto}, como você esta hoje no auge do seus ${idade}?`);
+
+alert(`Olá ${nomeCompleto}, como você esta hoje no auge do seus ${idade}?`);
